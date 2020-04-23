c48edb6b46b2d8383b2dc5240188e932bb855a70
Nokram <51248002+Nokram@users.noreply.github.com>
2020-04-23 19:13:34 +0200

    return to localhost

--- UMR2020/layers/layers.js
+++ UMR2020/layers/layers.js
@@ -2,7 +2,7 @@ var wms_layers = [];
 
 var lyr_DepHydro_0 = new ol.layer.Tile({
                             source: new ol.source.TileWMS(({
-                              url: "http://192.168.1.1:8080/geoserver/UMR-2020/wms?version%3D1.1.0%26layers%3DUMR-2020%3AUMR-2020%26bbox%3D-15033.043733948376%2C6019370.146298862%2C1346105.3951628036%2C7142683.034909542%26width%3D768%26height%3D633%26srs%3DEPSG%3A2154",
+                              url: "http://localhost:8080/geoserver/UMR-2020/wms?version%3D1.1.0%26layers%3DUMR-2020%3AUMR-2020%26bbox%3D-15033.043733948376%2C6019370.146298862%2C1346105.3951628036%2C7142683.034909542%26width%3D768%26height%3D633%26srs%3DEPSG%3A2154",
     attributions: ' ',
                               params: {
                                 "LAYERS": "UMR-2020",
